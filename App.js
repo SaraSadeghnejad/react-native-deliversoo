cb0b1f3b78d23acb96d4776f1e4f245b43d1c705
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2023-04-22 10:27:47 +0430

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import {  Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import HomeScreen from './screens/HomeScreen';
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+    <TailwindProvider>
+    <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </TailwindProvider>
+    </NavigationContainer>
+  );
+}
+
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+
+const HomeScreen = () => {aaaa
+    useLayoutEffect (()=>{
+        navigation.setOptions({
+
+        })
+    },[])
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
